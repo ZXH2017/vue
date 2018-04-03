8f8b0521499ce4d34f8d32d38d06237f1e4a4ff6
ZXH2017 <564407380@qq.com>
2018-04-03 12:39:26 +0800

    抽出header

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      tab!
+    </div>
+    <div class="content">
+      content!
+    </div>
+  </div>
 </template>
 
 <script>
+import header from './components/header/header.vue';
+export default{
+  components: {
+    'v-header': header
+  }
+};
 </script>
 
 <style>
