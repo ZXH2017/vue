5b49e1f2ea9c5262c24d77bb8b7eb9c87c11347d
ZXH2017 <564407380@qq.com>
2018-05-23 23:42:33 +0800

    add star component

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -32,14 +32,7 @@
     <div v-show="detailShow" class="detail">
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
-          <p>{{seller.bulletin}}</p>
+            <h1 class="name">{{seller.name}}</h1>
         </div>
       </div>
       <div class="detail-close">
@@ -245,10 +238,16 @@ export default {
 
     .detail-wrapper {
       min-height: 100%;
-
+      width:100%;
       .detail-main {
         margin-top: 64px;
         padding-bottom: 64px;
+        .name{
+          line-height :16px;
+          text-align :center;
+          font-size :16px;
+          font-weight :700;
+        }
       }
     }
 
