476a0cb9065ec996bf006bcb44076851caebb9af
ZXH2017 <564407380@qq.com>
2018-09-11 23:08:50 +0800

    update shopcart

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -3,29 +3,61 @@
       <div class="content">
           <div class="content-left">
               <div class="logo-wrapper">
-                  <div class="logo">
-                      <i class="icon-shopping_cart"></i>
+                  <div class="logo"  :class="{'highlight':totalCount>0}">
+                      <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
                   </div>
+                  <div class="num" v-show="totalCount>0">{{totalCount}}</div>
               </div>
-              <div class="price">0元</div>
+              <div class="price"  :class="{'highlight':totalPrice>0}">¥{{totalPrice}}</div>
               <div class="desc">另需配送费¥{{deliveryPrice}}元</div>
           </div>
-          <div class="content-right"></div>
+          <div class="content-right">
+              <div class="pay">
+                  ¥{{minPrice}}元起送
+              </div>
+          </div>
       </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 export default {
-  props:{
-     deliveryPrice:{
-         type:Number,
-         default:0
-     },
-     minPrice:{
-         type:Number,
-         default:0
-     } 
+  props: {
+    selectFoods: {
+      type: Array,
+      default() {
+        return [
+          {
+            price: 20,
+            count: 13
+          }
+        ];
+      }
+    },
+    deliveryPrice: {
+      type: Number,
+      default: 0
+    },
+    minPrice: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    totalPrice() {
+      let total = 0;
+      this.selectFoods.forEach(food => {
+        total += food.price * food.count;
+      });
+      return total;
+    },
+    totalCount() {
+      let count = 0;
+      this.selectFoods.forEach(food => {
+        count += food.count;
+      });
+      return count;
+    }
   }
 };
 </script>
@@ -43,6 +75,8 @@ export default {
     .content {
         display: flex;
         background: #141d27;
+        font-size: 0;
+        color: rgba(255, 255, 255, 0.4);
 
         .content-left {
             flex: 1;
@@ -67,11 +101,35 @@ export default {
                     text-align: center;
                     background: #2b343c;
 
+                    &.highlight {
+                        background: rgb(0, 160, 220);
+                    }
+
                     .icon-shopping_cart {
                         line-height: 44px;
                         font-size: 24px;
                         color: #80858a;
+
+                        &.highlight {
+                            color: #fff;
+                        }
                     }
+                }
+
+                .num {
+                    position: absolute;
+                    top: 0;
+                    right: 0;
+                    width: 24px;
+                    height: 16px;
+                    line-height: 16px;
+                    text-align: center;
+                    border-radius: 16px;
+                    font-size: 9px;
+                    color: #fff;
+                    font-weight: 700;
+                    background: rgb(240, 20, 20);
+                    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4);
                 }
             }
 
@@ -85,7 +143,10 @@ export default {
                 border-right: 1px solid rgba(255, 255, 255, 0.1);
                 font-size: 16px;
                 font-weight: 700;
-                color: rgba(255, 255, 255, 0.4);
+
+                &.highlight {
+                    color: #fff;
+                }
             }
 
             .desc {
@@ -93,7 +154,6 @@ export default {
                 vertical-align: top;
                 margin: 12px 0 0 12px;
                 line-height: 24px;
-                color: rgba(255, 255, 255, 0.4);
                 font-size: 10px;
             }
         }
@@ -101,6 +161,15 @@ export default {
         .content-right {
             flex: 0 0 105px;
             width: 105px;
+
+            .pay {
+                height: 48px;
+                line-height: 48px;
+                text-align: center;
+                font-size: 12px;
+                font-weight: 700;
+                background: #2b333b;
+            }
         }
     }
 }
